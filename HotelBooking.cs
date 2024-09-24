e5a2c8cc1862903562cfcb5feb09b913971b9438
Anton <anton.dahlstrom@hotmail.com>
2024-09-24 15:13:42 +0200

    completed step 3 with validation outside class

---
+++ HotelBooking.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheOOPHotel
+{
+    internal class HotelBooking
+    {
+        public string GuestName;
+        public DateTime StartDate;
+        public DateTime EndDate;
+        public int PriceTotal;
+        private int _price = 1250;
+        public HotelBooking(string guestName, DateTime startDate, int lengthOfStay) 
+        {   
+            this.GuestName = guestName;
+            this.StartDate = startDate;
+            DateTime endDate = startDate;
+            endDate = endDate.AddDays(lengthOfStay);
+            this.EndDate = endDate;
+            this.PriceTotal = this._price * lengthOfStay;
+        }
+        public void DisplayBookingInfo()
+        {
+            Console.WriteLine($"Gästnamn: {this.GuestName}");
+            Console.WriteLine($"Startdatum: {this.StartDate:d}");
+            Console.WriteLine($"Slutdatum: {this.EndDate:d}");
+            Console.WriteLine($"Pris: {this.PriceTotal:d}");
+        }
+        public void ChangeDuration(int days)
+        {
+            this.PriceTotal = days * this._price;
+            DateTime endDate = this.StartDate;
+            endDate = endDate.AddDays(days);
+            this.EndDate = endDate;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        HotelBooking bokning = Program.MakeBooking();
+
+        // Skriv ut information om bokningen med egenskaper
+        Console.WriteLine($"Gästnamn: {bokning.GuestName}");
+        Console.WriteLine($"Startdatum: {bokning.StartDate:d}");
+        Console.WriteLine($"Slutdatum: {bokning.EndDate:d}");
+        Console.WriteLine($"Pris: {bokning.PriceTotal:d}");
+
+        // Skriv ut information om bokningen med en metod istället
+        bokning.DisplayBookingInfo();
+    }
+    static HotelBooking MakeBooking()
+    {
+        Console.WriteLine("Ange ditt namn");
+        string name = Console.ReadLine();
+        DateTime startDate = GetValidDate();
+        int days;
+        string input;
+        do
+        {
+            Console.WriteLine("Hur många dagar vill du boka?");
+            input = Console.ReadLine();
+
+        } while (!int.TryParse(input, out days));
+        HotelBooking bokning = new HotelBooking(name, startDate, days);
+        return bokning;
+    }
+    static DateTime GetValidDate()
+    {
+        DateTime today = DateTime.Now;
+        DateTime startDate;
+        string input;
+        while (true)
+        {
+            Console.WriteLine("Ange datum i formatet åååå-mm-dd");
+            input = Console.ReadLine();
+            bool validDate = DateTime.TryParseExact(input, "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out startDate);
+            if (!validDate) { continue; }
+            if (startDate < today) {
+                Console.WriteLine("Datumet måste vara i framtiden.");
+                continue;
+            }
+            TimeSpan timeDifference = startDate - today;
+            TimeSpan year = new TimeSpan(365, 0, 0, 0);
+            if (timeDifference > year) {
+                Console.WriteLine("Man kan max boka ett år framåt.");
+                continue; }
+            break;
+        }
+        return startDate;
     }
 }

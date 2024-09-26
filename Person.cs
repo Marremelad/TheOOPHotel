925939965b3c60927462f8d33c0c641012cff7f6
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-09-26 11:26:56 +0200

    Add getter and setter for email.

--- Person.cs
+++ Person.cs
@@ -1,14 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace TheOOPHotel;
 
 public class Person
 {
+    // Class fields.
     private string? _name;
     private string? _email;
     private string? _phoneNumber;
-
-
+    
+    // Constructor.
     public Person(string? name, string? email, string? phoneNumber)
     {
         _name = name;
@@ -24,11 +26,34 @@ public class Person
         {
             if (string.IsNullOrEmpty(value))
             {
-                throw new ArgumentException("Name can not be empty");
+                throw new ArgumentException("Name can not be empty.");
             }
 
             _name = value;
 
+        }
+    }
+    
+    // Getter and setter for _email.
+    public string? Email
+    {
+        
+        get => _email;
+        set
+        {
+            
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Email can not be empty");
+            }
+            
+            Regex regex = new Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$"); // Check for valid email format with regular expression.
+            if (!regex.IsMatch(value))
+            {
+                throw new ArgumentException("Invalid email format");
+            }
+
+            _email = value;
         }
     }
 }
